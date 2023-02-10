96844c0d3a63822873e6af65037077536910959d
PGKKIN <93719216+PGKKIN@users.noreply.github.com>
2023-02-10 17:24:00 +0700

    Update lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double a[],int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if(((i+1)%M)==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+
+void randData(double a[],int N, int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = ((rand()%101)*0.01);
+    }
+}
+
+void findRowSum(const double *a,double *b,int N ,int M)
+{
+    for(int i = 0; i < N; i++)
+    {
+        b[i]=0;
+        for(int x=0;x<M;x++)
+        {
+            if(i>0) b[i]+=a[x+(M*i)];
+            else b[i]+=a[x];
+        }
+    }    
+}
+
+void findColSum(const double *a,double *b,int N ,int M)
+{
+    for(int i = 0; i < M; i++)
+    {
+        b[i]=0;
+        for(int x=0;x<M*N;x+=M)
+        {
+            if(i>0) b[i]+=a[x+i];
+            else b[i]+=a[x];
+        }
+    }    
+}
